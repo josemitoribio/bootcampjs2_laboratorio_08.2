80d2ecc61f32b85d035984eee7b9837912399b0d
Josemi Toribio <josemitoribio@gmail.com>
2023-11-09 20:06:36 +0100

    Laboratorio 08.1 corregido

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,8 @@ const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
     let activarProtocolo = false;
     for (let i = 0; i < pacientes.length; i++) {
         if (pacientes[i].frecuenciaCardiaca > 100 && pacientes[i].temperatura > 39) {
-         activarProtocolo = true;   
+         activarProtocolo = true;
+         break;  
         }
     }
     return activarProtocolo;
